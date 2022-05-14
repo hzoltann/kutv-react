ffb3e1b4b75a3cf6ea86c31d2b816fe7e9f5b0bd
hzoltann <huvelyesz@gmail.com>
2022-05-14 16:41:37 +0300

    Updates

---
+++ precache-manifest.21623355f301ae2c033196b27f36f0e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c4d1d6a38e09900ddee",
+    "url": "/kutv-react/static/css/main.442672fd.chunk.css"
+  },
+  {
+    "revision": "6c4d1d6a38e09900ddee",
+    "url": "/kutv-react/static/js/main.7dd34cbb.chunk.js"
+  },
+  {
+    "revision": "e34432f8b8632f540392",
+    "url": "/kutv-react/static/js/runtime~main.ffef9a51.js"
+  },
+  {
+    "revision": "2cbd812f4397420a61f9",
+    "url": "/kutv-react/static/js/2.e1aa25d7.chunk.js"
+  },
+  {
+    "revision": "ed44dc5d1d3df4597fbdd91513f53382",
+    "url": "/kutv-react/static/media/bgVideo.ed44dc5d.mp4"
+  },
+  {
+    "revision": "b6648f3daa58c7453b05fbf3475991b9",
+    "url": "/kutv-react/static/media/congrat.b6648f3d.svg"
+  },
+  {
+    "revision": "5c0716a97a6e47520ae512452aa51a21",
+    "url": "/kutv-react/static/media/deadline.5c0716a9.svg"
+  },
+  {
+    "revision": "c642be39a6cea7961b0e4a2adee4244b",
+    "url": "/kutv-react/static/media/fordulo.c642be39.svg"
+  },
+  {
+    "revision": "d3e734aba6f13b5f45ac3666f60f7a89",
+    "url": "/kutv-react/static/media/target.d3e734ab.svg"
+  },
+  {
+    "revision": "32ace8c9f7ce95308024e5a2336140c4",
+    "url": "/kutv-react/static/media/rule.32ace8c9.svg"
+  },
+  {
+    "revision": "ba138f32949b18ae3e093d4cf5226883",
+    "url": "/kutv-react/static/media/car.ba138f32.svg"
+  },
+  {
+    "revision": "2788826dc4425475b0df0381ed2763b4",
+    "url": "/kutv-react/index.html"
+  }
+];
